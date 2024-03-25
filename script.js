f018aa6ac805bc8d7818a54510f51898f3960130
Guyot <thomasguyot31@live.fr>
2024-03-25 23:56:41 +0100

    Last Maj

--- script.js
+++ script.js
@@ -62,7 +62,23 @@ var max = $('#c > li').length;
 		});
 	});
 
-
-
+	let sections = document.querySelectorAll('section');
+	let navLinks = document.querySelectorAll('header nav a');
+	
+	window.addEventListener('scroll', () => {
+		sections.forEach(sec => {
+			let top = window.scrollY;
+			let offset = sec.offsetTop - 100;
+			let height = sec.offsetHeight;
+			let id = sec.getAttribute('id');
+	
+			if (top >= offset && top < offset + height) {
+				navLinks.forEach(link => {
+					link.classList.remove('active');
+				});
+				document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+			}
+		});
+	});
 
 	
