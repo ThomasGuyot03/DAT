7a96783fa25c7c095054be384f53e389901fb0b3
Guyot <thomasguyot31@live.fr>
2024-05-30 14:54:42 +0200

    Modif script remplissage champs

--- contact.js
+++ contact.js
@@ -1,21 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const navbar = document.getElementById('navbar');
-
-    if (menuIcon && navbar) {
-        menuIcon.addEventListener('click', function () {
-            navbar.classList.toggle('active');
-        });
-    }
-});
-
-
 function envoyerEmail() {
-
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const message = document.getElementById('message').value;
+
+    // Vérification des champs obligatoires
+    if (!name || !email || !phone || !message) {
+        // Affichage d'un message d'erreur si un champ obligatoire est vide
+        alert("Veuillez remplir tous les champs obligatoires.");
+        return; // Arrêt de la fonction si un champ est vide
+    }
+
     // Initialisation de EmailJS 
     emailjs.init('JOTvNGFjrUQfQG57r');
     // Envoi des données par e-mail via EmailJS
@@ -32,3 +27,14 @@ function envoyerEmail() {
         document.getElementById('resultat').innerHTML = "Une erreur s'est produite lors de l'envoi de l'e-mail.";
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.getElementById('navbar');
+
+    if (menuIcon && navbar) {
+        menuIcon.addEventListener('click', function () {
+            navbar.classList.toggle('active');
+        });
+    }
+});
